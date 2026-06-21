9d8eba2f886d7e9fb2bc396c37f8a6eb1b234a23
bhuvanesh <bhuvaneshm2759@gmail.com>
2026-06-21 15:27:52 +0530

    fix cors allow all

--- njdbe/nanjanad_fc/settings.py
+++ njdbe/nanjanad_fc/settings.py
@@ -117,9 +117,10 @@ SIMPLE_JWT = {
 }
 
 # CORS - allow React dev server (open for development)
-CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = ["https://nanjand-fc-5bz5-7l1z0ovox-rb398573-4724s-projects.vercel.app",
-                        ]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://nanjand-fc-5bz5-b416dfo03-rb398573-4724s-projects.vercel.app",
+]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = [
     "accept",
