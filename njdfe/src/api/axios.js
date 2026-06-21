ea692e2418964ebc86f94ef5e602b243c043c628
bhuvanesh <bhuvaneshm2759@gmail.com>
2026-06-21 15:15:19 +0530

    hardcode backend url

--- njdfe/src/api/axios.js
+++ njdfe/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000/api";
+const BASE = "https://nanjanad-fc-backend.onrender.com/api";
 const api = axios.create({ baseURL: BASE });
 api.interceptors.request.use((c) => {
   const t = localStorage.getItem("access_token");
